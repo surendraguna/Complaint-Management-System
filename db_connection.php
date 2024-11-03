ab07f65ef8958e8f5527ae36abd7deca932268d3
surendra_guna <surendraguna2812@gmail.com>
2024-11-03 16:16:16 +0530

    change db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost"; 
-$username = "root"; 
-$password = "Sure@1234";
-$database = "CRP"; 
+$servername = ""; 
+$username = "";
+$password = "";
+$database = "";
 
 
 $conn = mysqli_connect($servername, $username, $password, $database);
